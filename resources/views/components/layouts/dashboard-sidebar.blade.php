841e57f7d73a195ef7d4da6a3bb3df7ad3b87140
Zoltán Tamás Szabó <zoli.szabok@gmail.com>
2026-03-02 17:07:06 +0100

    feat: Comment out the Administration section in the dashboard sidebar

--- resources/views/components/layouts/dashboard-sidebar.blade.php
+++ resources/views/components/layouts/dashboard-sidebar.blade.php
@@ -207,7 +207,7 @@
         </div>
 
         {{-- Administration section --}}
-        @auth
+       {{--  @auth
             <div>
                 <h3 class="px-3 text-xs font-semibold text-gray-400 uppercase tracking-wider mb-2">{{ __('Administration') }}</h3>
                 <ul class="space-y-1">
@@ -256,7 +256,7 @@
                     </li>
                 </ul>
             </div>
-        @endauth
+        @endauth --}}
 
         {{-- Intrastat section --}}
         <div>
